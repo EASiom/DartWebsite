5819fe0544d0ab6a326d0ecfb39b5cdb1117999d
emilian <emilian.rebrovi1@gmail.com>
2024-04-10 13:15:49 +0200

    renamed stuff

--- Dart_Backend/main.go
+++ Dart_Backend/main.go
@@ -29,13 +29,6 @@ func setupRouter() *gin.Engine {
 		}
 	})
 
-	// Authorized group (uses gin.BasicAuth() middleware)
-	// Same than:
-	// authorized := r.Group("/")
-	// authorized.Use(gin.BasicAuth(gin.Credentials{
-	//	  "foo":  "bar",
-	//	  "manu": "123",
-	//}))
 	authorized := r.Group("/", gin.BasicAuth(gin.Accounts{
 		"foo":  "bar", // user:foo password:bar
 		"manu": "123", // user:manu password:123
